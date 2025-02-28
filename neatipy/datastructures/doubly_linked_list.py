f5f5a3066f6f554da79d4108de86ff140559aa10
Vilppu Tiilikainen <blaanblaa123@gmail.com>
2025-02-28 17:10:54 +0200

    Add remove and insert methods to DoublyLinkedList; implement LRUCache class

--- neatipy/datastructures/doubly_linked_list.py
+++ neatipy/datastructures/doubly_linked_list.py
@@ -12,6 +12,7 @@ class DoublyLinkedList():
     
 
     def remove_from_tail(self)->None:
+        """Remove from tail"""
         new_tail=self.tail.prev
         if new_tail is None:
             self.head=None
@@ -21,9 +22,32 @@ class DoublyLinkedList():
         self.tail=new_tail
         self.tail.next=None
         self.size-=1
+    
+    def remove(self,node:Node)->None:
+        """Remove node from list, assumes it exists. In this context we will check its existence using hashmap as this class will be used in LRU cache"""
+        if self.is_empty():
+            return
+        if self.tail is node:
+            self.tail=node.prev
+            if self.tail:
+                self.tail.next=None
+            else:
+                self.head=None
+        elif self.head is node:
+            self.head=node.next
+            if self.head:
+                self.head.prev=None
+            else:
+                self.tail=None
+        else:
+            node.prev.next=node.next
+            node.next.prev=node.prev
+        self.size-=1
+        
         
 
     def insert_to_head(self,node:Node)->None:
+        """Insert node to head"""
         if self.is_empty():
             self.head=node
             self.tail=self.head
---
+++ neatipy/datastructures/lru_cache.py
@@ -0,0 +1,3 @@
+class LRUCache():
+    def __init__(self,size:int=128):
+        self.size=size
